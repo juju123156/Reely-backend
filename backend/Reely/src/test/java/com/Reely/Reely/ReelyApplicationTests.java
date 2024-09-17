81483bcf93569e8fb59be401cfcd8114fa5ea5ce
jh.kim <fnqllove11@naver.com>
2024-09-17 20:35:31 +0900

    test 수정

--- backend/Reely/src/test/java/com/Reely/Reely/ReelyApplicationTests.java
+++ backend/Reely/src/test/java/com/Reely/Reely/ReelyApplicationTests.java
@@ -2,7 +2,7 @@ package com.Reely.Reely;
 
 //import org.junit.jupiter.api.Test;
 
-import org.junit.jupiter.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
