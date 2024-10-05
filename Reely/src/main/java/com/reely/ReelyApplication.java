6d6d1d42c68ccb8c37be3e1eef89f602c6db7bcb
jh.kim <fnqllove11@naver.com>
2024-10-05 13:44:50 +0900

    스프링시큐리티 임시로 막아둠

--- Reely/src/main/java/com/reely/ReelyApplication.java
+++ Reely/src/main/java/com/reely/ReelyApplication.java
@@ -2,8 +2,10 @@ package com.reely;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+// 스프링 시큐리티 임시로 막아둠
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class ReelyApplication {
 
 	public static void main(String[] args) {
