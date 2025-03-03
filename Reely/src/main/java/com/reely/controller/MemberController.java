983fc0d22e6f6caba68df475a126c7fb3349f546
pearyoon <yooon0769@gmail.com>
2025-03-03 20:38:33 +0900

    일단 테스트 코드

--- Reely/src/main/java/com/reely/controller/MemberController.java
+++ Reely/src/main/java/com/reely/controller/MemberController.java
@@ -1,10 +1,12 @@
 package com.reely.controller;
 
 import com.reely.dto.MemberDto;
+import com.reely.security.CustomUserDetails;
 import com.reely.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -30,9 +32,9 @@ public class MemberController {
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
+    // 테스트 코드 추후 삭제
     @GetMapping("/test")
-    public ResponseEntity<String> test() {
-        return new ResponseEntity<>("성공", HttpStatus.OK);
+    public ResponseEntity<String> test(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        return new ResponseEntity<>("[성공]사용자 pk : " + userDetails.getUsername(), HttpStatus.OK);
     }
-
 }
