57d17bea65c56e516f971027eb89fa3f52e596f3
juju123156 <fnqllove11@naver.com>
2025-05-16 17:49:49 +0900

    데이터 세팅 에러 수정

--- Reely/src/main/java/com/reely/service/MovieService.java
+++ Reely/src/main/java/com/reely/service/MovieService.java
@@ -2,6 +2,7 @@ package com.reely.service;
 
 import org.springframework.stereotype.Service;
 
+import com.reely.dto.MovieDto;
 import com.reely.dto.SpotifyDto;
 import com.reely.dto.SpotifyDto.SpotifyAlbumTracksDto;
 
@@ -9,4 +10,14 @@ public interface MovieService {
 
     SpotifyDto getMovieOst(String movieNm);
     SpotifyAlbumTracksDto getSpotifyAlbumTracks(String albumId, int limit);
+    int insertFileInfo(MovieDto movieDto);
+
+
+
+
+
+
+
+
+
 }
